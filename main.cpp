81b56878a1921135c1a8dc2e3a7d4856d55a42c2
yassirprograma <dj.jicara@gmail.com>
2020-05-22 00:00:59 -0500

    Declaramos tablero con memoria dinamica

--- main.cpp
+++ main.cpp
@@ -7,6 +7,22 @@
 
 int main()
 {
+    int *tablero;
+
+	/*vector dinámico que guarda la solución
+	 ( numero de columna en la que está la reina ) :*/
+	tablero = (int*)malloc(sizeof(int));
+
+	if(tablero==NULL){
+            /*NO PUDO ASIGNAR ESPACIO*/
+            printf("\n No se pudo reservar espacio en memoria memoria \n\n");
+	}
+	else{
+            /*HUBO ESPACIO SUFICIENTE*/
+            printf("\n Espacio de memoria reservado correctamente \n\n");
+	}
+
+	system("pause");
 
     return 0;
 }
